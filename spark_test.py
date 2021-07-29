f66c817265f1ef57fe8b73bc43d887ee3d37bf2f
houguanyu <houguanyu@bbdservice.com>
2021-07-29 10:49:51 +0800

    'test'

--- spark_test.py
+++ spark_test.py
@@ -9,26 +9,18 @@ def getSparkSession():
     conf = SparkConf()
     conf.setMaster('yarn-client')
     # conf.setMaster('local[*]')
-    # conf.setAppName("My app")
-    spark = SparkSession.builder.appName("My APP").config(conf=conf).enableHiveSupport().getOrCreate
-    # spark = SparkContext(conf=conf)
+    conf.setAppName("My app")
+    # spark = SparkSession.builder.appName("My APP").config(conf=conf).enableHiveSupport().getOrCreate
+    spark = SparkContext(conf=conf)
     return spark
 
-spark = getSparkSession()
+
 if __name__ == '__main__':
+    spark = getSparkSession()
     rdd = spark.textFile('hdfs:///data1/wordcount.txt')
     # rdd = spark.textFile('wordcount.txt')
     wc = rdd.flatMap(lambda x: re.split('[，。：]', x)).repartition(5)
     wc.saveAsTextFile('hdfs:///data1/result.txt')
     spark.stop()
-
-
-
-
-
-
-
-
-
 
 
